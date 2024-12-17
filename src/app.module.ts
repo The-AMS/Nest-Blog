c75b86cd559971ce49f5193ca1573754d872b949
the-ams <moeinati.79@gmail.com>
2024-12-17 14:43:47 +0330

    1.0 strcture design

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [AuthModule, PostsModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
